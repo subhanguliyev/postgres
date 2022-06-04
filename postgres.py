9cc6951b3ed8e4c327115aed495c67e23f79294a
Subhan Guliyev <squadiloglu@gmail.com>
2022-06-04 21:58:49 +0400

    postgres, workflow

--- postgres.py
+++ postgres.py
@@ -1,23 +1,22 @@
-# information from pg admin properties
-DB_NAME = 'persons'
-DB_HOST = 'localhost'
-DB_USER = 'postgres'
-DB_PASS = 'password'
-
 import psycopg2
 
-conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+# information from pg properties
+conn = psycopg2.connect(dbname='persons', user='postgres', password='password', host='localhost')
 
 cur = conn.cursor()
-
+# creating DB, table, columns
 # cur.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, skills VARCHAR);")
 
+# adding data to columns
 # cur.execute("INSERT INTO users (name) VALUES(%s)", ("Mary",))
 
+# SELECT data from table
 cur.execute("SELECT * FROM users")
 
+# update date to columns
 # cur.execute("UPDATE users SET skills=(%s) WHERE id=6", ("officiant",))
 
+# output of table
 print(cur.fetchall())
 conn.commit()
 cur.close()
